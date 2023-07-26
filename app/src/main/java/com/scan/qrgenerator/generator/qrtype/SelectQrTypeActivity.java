ebadbc1f69f0e50968c5ce4e460b451087ff0231
Shibin <shibinprk@gmail.com>
2023-07-26 17:55:52 +0530

    v1.3 new option

---
+++ app/src/main/java/com/scan/qrgenerator/generator/qrtype/SelectQrTypeActivity.java
@@ -0,0 +1,16 @@
+package com.scan.qrgenerator.generator.qrtype;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.scan.qrgenerator.R;
+
+public class SelectQrTypeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_qr_type);
+    }
+
+
+}
